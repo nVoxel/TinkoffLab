f4372392c2c99815b46a131936565e445a8be818
nVoxel <nvoxel@outlook.com>
2023-03-22 23:16:06 +0300

    Remove redundant responses

--- app/src/main/java/com/voxeldev/tinkofflab/data/network/responses/GetSlotsResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.voxeldev.tinkofflab.data.network.responses
-
-import com.google.gson.annotations.SerializedName
-import com.voxeldev.tinkofflab.data.network.models.TimeSlotModel
-
-data class GetSlotsResponse(
-    @SerializedName("")
-    val slots: List<TimeSlotModel>
-)
