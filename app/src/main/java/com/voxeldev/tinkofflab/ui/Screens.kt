73c5f9ce45b0d3c92e53a24b1c2b13b3fdf32506
nVoxel <nvoxel@outlook.com>
2023-03-23 15:43:48 +0300

    Replace CardView to ShapeableImageView in cart screen, fix manifest

--- app/src/main/java/com/voxeldev/tinkofflab/ui/Screens.kt
+++ app/src/main/java/com/voxeldev/tinkofflab/ui/Screens.kt
@@ -1,11 +1,12 @@
 package com.voxeldev.tinkofflab.ui
 
 import com.github.terrakok.cicerone.androidx.FragmentScreen
+import com.voxeldev.tinkofflab.ui.cart.CartFragment
 import com.voxeldev.tinkofflab.ui.delivery.deliverytype.DeliveryTypeFragment
 
 @Suppress("FunctionName")
 object Screens {
 
     fun DeliveryType() = FragmentScreen { DeliveryTypeFragment() }
-
+    fun Cart() = FragmentScreen { CartFragment() }
 }
