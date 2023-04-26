d1ca183b3cd830c75d27b9eaa9e96a9e4f899176
marat <mingazov.marat2003@yandex.ru>
2023-04-26 14:48:48 +0300

    implemented confirmation screen

--- app/src/main/java/com/voxeldev/tinkofflab/ui/delivery/addressautofill/AddressAutofillViewModel.kt
+++ app/src/main/java/com/voxeldev/tinkofflab/ui/delivery/addressautofill/AddressAutofillViewModel.kt
@@ -58,6 +58,7 @@ class AddressAutofillViewModel @Inject constructor(
             .onEach { query ->
                 getAddressSuggestionsUseCase(query!! to locale, viewModelScope) { either ->
                     either.fold(::handleException) {
+                        _loading.postValue(false)
                         _suggestions.postValue(it)
                     }
                     _loading.postValue(false)
