8d2c2c1142bdc25015ce9af7a928c3a4eda0d45d
nVoxel <nvoxel@outlook.com>
2023-04-27 15:56:01 +0300

    Fix app crash in orders screen

--- app/src/main/java/com/voxeldev/tinkofflab/ui/orders/OrdersAdapter.kt
+++ app/src/main/java/com/voxeldev/tinkofflab/ui/orders/OrdersAdapter.kt
@@ -10,7 +10,6 @@ import com.voxeldev.tinkofflab.domain.models.expressapi.OrderModel
 import com.voxeldev.tinkofflab.domain.models.expressapi.TimeSlotModel
 import com.voxeldev.tinkofflab.ui.utils.toRelativeString
 import java.time.LocalDate
-import java.time.format.DateTimeFormatter
 
 
 class OrdersAdapter(
@@ -47,14 +46,19 @@ class OrdersAdapter(
 
         private fun timeSlotToString(resources: Resources, timeSlot: TimeSlotModel): String {
             val currentDate = LocalDate.now()
-            val timeSlotDate = LocalDate.parse(timeSlot.date, DateTimeFormatter.ISO_DATE)
 
-            return resources.getString(
-                R.string.order_datetime_placeholder,
-                timeSlotDate.toRelativeString(currentDate, resources),
-                timeSlot.timeFrom,
-                timeSlot.timeTo
-            )
+            runCatching {
+                LocalDate.parse(timeSlot.date)
+            }.onSuccess {
+                return resources.getString(
+                    R.string.order_datetime_placeholder,
+                    it.toRelativeString(currentDate, resources),
+                    timeSlot.timeFrom,
+                    timeSlot.timeTo
+                )
+            }
+
+            return timeSlot.date
         }
     }
 }
